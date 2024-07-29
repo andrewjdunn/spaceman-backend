423bb1cc47e0873a3860bd699f42ff1c0558b1c2
Andy <dunnad@gmail.com>
2024-07-29 22:11:12 +0100

    DM-26 Added tutorial code for adding to firestore

--- main.go
+++ main.go
@@ -1,33 +1,75 @@
 package main
+
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"cloud.google.com/go/firestore"
 )
 
+func createClient(ctx context.Context) *firestore.Client {
+	// Sets your Google Cloud Platform project ID.
+	projectID := "hciware-spaceman"
+
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	// Close client when done with
+	// defer client.Close()
+	return client
+}
 
 // indexHandler responds to requests with our greeting.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
+		http.NotFound(w, r)
+		return
 	}
 	fmt.Fprint(w, "Hello, World!")
 }
 
-
-func main() {
+func addRootHandler() {
 	http.HandleFunc("/", indexHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-			port = "8080"
-			log.Printf("Defaulting to port %s", port)
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
 	}
 
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
+}
+
+func writeAUser(client *firestore.Client, ctx context.Context, userMap map[string]interface{}) {
+	_, _, err := client.Collection("users").Add(ctx, userMap)
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+}
+
+func main() {
+
+	// Get a Firestore client.
+	ctx := context.Background()
+	client := createClient(ctx)
+	defer client.Close()
+
+	writeAUser(client, ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815})
+
+	writeAUser(client, ctx, map[string]interface{}{
+		"first":  "Alan",
+		"middle": "Mathison",
+		"last":   "Turing",
+		"born":   1912,
+	})
 }
